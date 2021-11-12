8101b400dedcb2f44ccb83c66cf5fbee0aaab125
Ellie575 <elliekubo@hotmail.com>
2021-11-12 17:28:37 +1100

    Added Github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h2>Hello from App!</h2>
+      <p>
+        This app was coded by Ellie Kubo and is open-sourced on{" "}
+        <a href="https://github.com/Ellie575/react-app" target="_blank">
+          Github
+        </a>
+      </p>
     </div>
   );
 }
