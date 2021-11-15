55641acefab20eda57a01f946ab8fa100163c7e7
Ellie575 <elliekubo@hotmail.com>
2021-11-15 21:17:01 +1100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Hello from App!</h2>
-      <p>
-        This app was coded by Ellie Kubo and is open-sourced on{" "}
-        <a href="https://github.com/Ellie575/react-app" target="_blank">
-          Github
-        </a>
-      </p>
+      <div className="container">
+        <h2>Hello from App!</h2>
+        <p>
+          This app was coded by Ellie Kubo and is open-sourced on{" "}
+          <a
+            href="https://github.com/Ellie575/react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
