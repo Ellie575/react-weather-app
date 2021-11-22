8e97104da7ebe2cb71be09c103e9d81852ee69ee
Ellie575 <elliekubo@hotmail.com>
2021-11-23 00:31:20 +1100

    Added unit conversion

--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,25 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-
 import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false} );
+  const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
-    
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
       date: new Date(response.data.dt * 1000),
-      wind: response.data.wind.speed, 
+      wind: response.data.wind.speed,
       city: response.data.name,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-    }
-      );
-    
+      icon: response.data.weather[0].icon,
+    });
   }
 
-  function search(){
+  function search() {
     let apiKey = "56e0818fd22a5efa4a6c31eab2ac96a0";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
@@ -34,37 +30,37 @@ export default function Weather(props) {
   }
 
   function handleCityChange(event) {
-setCity(event.target.value);
-search();
-  }
-    
-
-if (weatherData.ready) {
-  return (
-    <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              onChange={handleCityChange}
-            />
-          </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary w-100" />
-          </div>
-        </div>
-      </form>
-      <WeatherInfo data={weatherData}/>
-
-      
-    </div>
-  );
-  }
-  else {
+    setCity(event.target.value);
     search();
-    return "Loading...";  
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
   }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,28 +1,29 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    return (
-        <div className="WeatherInfo">
-        <div className="row mt-3">
+  return (
+    <div className="WeatherInfo">
+      <div className="row mt-3">
         <div className="col-6">
-        <WeatherIcon code={props.data.icon} alt={props.data.description}/>
-          <span className="temperature">{Math.round(props.data.temperature)}</span> 
-          <span className="unit">°C</span>
-          </div>
-          
-        
+          <WeatherIcon code={props.data.icon} alt={props.data.description} />
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+
         <div className="col-6">
           <h2>{props.data.city}</h2>
           <ul>
-            <li><FormattedDate date={props.data.date}/></li>
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
             <li className="text-capitalize">{props.data.description}</li>
             <li>{props.data.humidity}%</li>
             <li>{props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
-      </div>
-    );
+    </div>
+  );
 }
